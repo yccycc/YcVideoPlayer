47e366e118572333824d7828a185b06f6f577f6c
ycc <772878320@qq.com>
2015-12-18 07:46:17 +0800

    Initial commit

---
+++ app/src/main/java/redtube/mkteck/com/ycvideoplayer/MainActivity.java
@@ -0,0 +1,38 @@
+package redtube.mkteck.com.ycvideoplayer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainActivity extends AppCompatActivity {
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSurfaceView = (SurfaceView) findViewById(R.id.sf);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.sexy_love);
+                mediaPlayer.setDisplay(mSurfaceHolder);
+                mediaPlayer.start();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mediaPlayer.release();
+            }
+        });
+    }
+}
